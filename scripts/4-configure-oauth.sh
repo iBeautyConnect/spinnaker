751d073c47a5142dcf93db6a74114f58c805edca
admin <admin@admins-iMac.local>
2021-04-23 17:19:56 -0500

    first commit

---
+++ scripts/4-configure-oauth.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# env flags that need to be set:
+CLIENT_ID=6d4a9a1a0d9093864d68
+CLIENT_SECRET=d06bbc028fb0aa8550b82e69dbb3c3933ec18aa0
+PROVIDER=github
+REDIRECT_URI=http://167.172.156.16:8084/login
+
+set -e
+
+if [ -z "${CLIENT_ID}" ] ; then
+  echo "CLIENT_ID not set"
+  exit
+fi
+if [ -z "${CLIENT_SECRET}" ] ; then
+  echo "CLIENT_SECRET not set"
+  exit
+fi
+if [ -z "${PROVIDER}" ] ; then
+  echo "PROVIDER not set"
+  exit
+fi
+if [ -z "${REDIRECT_URI}" ] ; then
+  echo "REDIRECT_URI not set"
+  exit
+fi
+
+MY_IP=`curl -s ifconfig.co`
+
+hal config security authn oauth2 edit \
+  --client-id $CLIENT_ID \
+  --client-secret $CLIENT_SECRET \
+  --provider $PROVIDER
+hal config security authn oauth2 enable
+
+hal config security authn oauth2 edit --pre-established-redirect-uri $REDIRECT_URI
+
+hal config security ui edit \
+    --override-base-url http://${MY_IP}:9000
+
+hal config security api edit \
+    --override-base-url http://${MY_IP}:8084
